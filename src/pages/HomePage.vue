7e36e03ee5edcc5c8b028753eba756fea29091d5
Vyacheslav Bogdanov <sbhlrsa@gmail.com>
2025-11-19 08:40:12 +0400

    fix main

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -9,9 +9,6 @@
 
 <script setup lang="ts">
 import HeroSection from '@/components/HeroSection.vue';
-import ServicesSection from '@/components/ServicesSection.vue';
-import ProductsSection from '@/components/ProductsSection.vue';
-import ContactForm from '@/components/ContactForm.vue';
 </script>
 
 <style scoped>
